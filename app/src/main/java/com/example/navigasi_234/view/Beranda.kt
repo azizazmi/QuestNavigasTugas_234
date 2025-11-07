eb027b61d2687119bf0a3a8fe7b19404218e2b11
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-07 16:25:51 +0700

    feat: add spacer

--- app/src/main/java/com/example/navigasi_234/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi_234/view/Beranda.kt
@@ -3,7 +3,9 @@ package com.example.navigasi_234.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -15,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.navigasi_234.R
 
@@ -35,6 +38,7 @@ fun Beranda(OnSubmitBtnClick:()-> Unit) {
                 fontFamily = FontFamily.Serif,
                 fontSize = 40.sp,
                 color = Color(0xFF58b4f5))
+            Spacer(modifier = Modifier.height(height = 30.dp))
         }
 
     }
