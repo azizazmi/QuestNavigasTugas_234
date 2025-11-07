059f53d407caebb6b5c28fa1229aa030a6459a53
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-07 17:19:05 +0700

    refactor: add @OptIn(ExperimentalMaterial3Api::class

--- app/src/main/java/com/example/navigasi_234/view/List.kt
+++ app/src/main/java/com/example/navigasi_234/view/List.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -16,6 +17,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.navigasi_234.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListPeserta(
     onBackToBerandaClick:()-> Unit,
