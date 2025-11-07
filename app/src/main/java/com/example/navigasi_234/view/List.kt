0123fca565dd43e17a54ae907cf1c6e7d2d6754e
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-07 17:17:00 +0700

    feat: add scaffold dan layout column utama

--- app/src/main/java/com/example/navigasi_234/view/List.kt
+++ app/src/main/java/com/example/navigasi_234/view/List.kt
@@ -1,6 +1,18 @@
 package com.example.navigasi_234.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.navigasi_234.R
 
@@ -15,5 +27,21 @@ fun ListPeserta(
         Pair(first = stringResource(R.string.status), second = "Lajang"),
         Pair(first = stringResource(R.string.alamat), second = "Yogyakarta")
     )
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text=stringResource(R.string.list), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(R.color.purple_200))
+            )}
+    ) {
+        isiRuang->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top)
+        {
 
+        }
+    }
 }
